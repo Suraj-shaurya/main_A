b15c3c78d51de103800e0e67f1e073637a60663c
Suraj singh <surajshauryachaudhary@gmail.com>
2025-07-14 10:14:16 +0530

    us-rg added

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -4,3 +4,8 @@ module "resource_group_name" {
   resource_group_location = "centralindia"
 }
 
+module "resource_group_name" {
+  source                  = "../MODULES/azurerm_resource_group"
+  resource_group_name     = "todoapp_suraj/102/rg-us"
+  resource_group_location = "Us west"
+}
